6d591fc3a4ef17b862e7e41a0127ea42126c8d75
Maros <marossmrek@gmail.com>
2017-04-11 20:04:29 +0200

    User regitration - backend

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-const express = require('express');
-const router  = express.Router();
+const express  = require('express');
+const router   = express.Router();
+const passport = require('passport');
+const jwt      = require('jsonwebtoken');
+const User     = require('../models/users');
 
 //register
-router.get('/register', function(req, res, next){
-	res.send("REGISTER");
+router.post('/register', function(req, res, next){
+	
+	let newUser = new User({
+		name:req.body.name,
+		email:req.body.email,
+		username:req.body.username,
+		password:req.body.password
+	});
+
+	User.addUser(newUser, function(err, user){
+		if(err){
+			res.json({success: false, msg:"User registration failed"});
+		} else {
+			res.json({success:true, msg:"Registration success"});
+		}
+	});
 });
 
 //auth
@@ -14,11 +31,6 @@ router.post('/autehticate', function(req, res, next){
 //profile
 router.get('/profile', function(req, res, next){
 	res.send("profile");
-});
-
-//validate
-router.get('/validate', function(req, res, next){
-	res.send("validate");
 });
 
 module.exports = router;
