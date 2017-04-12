67009b4687d123e2ebc821a0ac8ab1c621eda6dd
Maros <marossmrek@gmail.com>
2017-04-12 15:11:32 +0200

    Auth service, User Registration

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {FlashMessagesModule} from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -10,6 +11,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+import { ValidateService } from './services/validate.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -29,9 +32,10 @@ import { AppRoutingModule }     from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  constructor(private validateS : ValidateService, private flashMessage:FlashMessagesService ){
 
-  ngOnInit() {
+  }
+
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  onRegisterSubmit(){
+
+  	const user = {
+  		name: this.name,
+  		username: this.username,
+  		email: this.email,
+  		password: this.password
+  	};
+
+  	if(!this.validateS.validateRegister(user)){
+  		this.flashMessage.show("Please insert all field", {cssClass:"alert-danger", timeout:3000});
+  		return false;
+  	};
+
+  	if(!this.validateS.validateEmail(user.email)){
+  		this.flashMessage.show("Wrong email", {cssClass:"alert-danger", timeout:3000});
+  		return false;
+  	};
+
   }
 
 }
