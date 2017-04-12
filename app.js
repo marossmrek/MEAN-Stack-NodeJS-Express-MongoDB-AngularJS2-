264803dc252553a7f0cc3966ddb82ac64180259d
Maros <marossmrek@gmail.com>
2017-04-12 10:59:52 +0200

    API Authetications and token

--- app.js
+++ app.js
@@ -1,44 +1,54 @@
-const express    = require('express');
-const path       = require('path');
+const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-const cors       = require('cors');
-const passport   = require('passport');
-const mongoose   = require('mongoose');
-const users      = require('./routes/users.js');
-const config     = require('./config/database.js');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
 
-// express app inicialization
+// Connect To Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
+
 const app = express();
 
-//connection to db
-mongoose.connect(config.database);
-//on connected
-mongoose.connection.on('connected', function(){
-	console.log("Connection on db " + config.database);
-});
-//on error
-mongoose.connection.on('error', function(e){
-	console.log("database error " + e);
-});
+const users = require('./routes/users');
 
-//CORS middleware
+// Port Number
+const port = 3000;
+
+// CORS Middleware
 app.use(cors());
 
-//BodyParser middlewar
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
 app.use(bodyParser.json());
 
-//set up routing
-app.use('/users',users); 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
-//set static folder
-app.use(express.static(path.join(__dirname, "public")));
+require('./config/passport')(passport);
 
-//start, dummy request to homepage
-app.get('', function(req, res){
-	res.send("Invalid endpoint");
-})
+app.use('/users', users);
 
-//server listen 
-app.listen(3000, function(){
-	console.log("App listen on port number 3000");
-})
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
+});
